1700f12f1c5680015de3a6cc466be1ef4006152d
Daniel Gockel <daniel.gockel@tum.de>
2022-07-30 04:44:16 +0200

    new program in plotly

--- README.md
+++ README.md
@@ -1,30 +1,16 @@
-# Bitcoin annual returns
-This simple Python script shows the annual bitcoin price performance (yearly percentage change). 
+# Bitcoin annual returns:
+This simple Python script shows the average annual bitcoin price performance (yearly percentage change). 
 
-The average price is determined each year. 
-For the performance the average price of the current year is divided by the average price of the last year.
-The last row shows the average percentage change per year (performance).
+The calculation used is `yearly_performance = nth_root_of(current_price/historic_price)`
 
-`python annulize_price.py` at 15 June 2017
-Output:
-```
-** Bitcoin yearly performance **
-year .. price end of year .. avg for year .. performance
-2010    0.299999              0.167    0.0
-2011    4.995000              6.054    35.242
-2012    13.590000              8.472    0.399
-2013    739.100000              188.786    21.284
-2014    317.400000              525.33    1.783
-2015    428.000000              271.828    -0.483
-2016    952.156375              566.383    1.084
-2017    2447.041563              1363.88    1.408
-total days:  1247
-first day:  2010-08-18
-last day:  2017-06-14
-average return per year:  867.38575 %
-```
+Demo:
+<img src='demo_pic.png'>
+The slider can be adjusted to see, how the annual return goes down over time. 
 
+The script requires pandas, dash and plotly packages.
 
 For a local copy: `git clone https://github.com/dancodery/bitcoin-annual-returns.git`
+
+Please give me a star if you like this repo ;=)
 
 Daniel Gockel © daniel@gockel.co
--- annulize_price.py
+++ annulize_price.py
@@ -1,79 +1,104 @@
 """
 
-This simple Python script shows the annual bitcoin price performance. 
+This simple Python script shows the annual bitcoin price performance (also known as annual Return on Investment).
 
-The average price is determined each year. 
-For the performance the average price of the current year is divided by the average price of the last year.
-The last row shows the average percentage change per year (performance).
+The average price is determined by using the nth root.
 
 __author__ = "Daniel Gockel"
-__copyright__ = "Copyright 2017"
+__copyright__ = "Copyright 2022"
 __email__ = "daniel@gockel.co"
 """
 
-import urllib2
+import urllib.request
+from datetime import datetime, date
+import pandas as pd
+import os.path
+from dateutil.relativedelta import relativedelta
+from dash import Dash, dcc, html, Input, Output
+import plotly.graph_objs as go
 
-url = "https://blockchain.info/charts/market-price?showDataPoints=false&timespan=all&show_header=true&daysAverageString=1&scale=0&format=csv&address="
+app = Dash(__name__)
 
-stream = urllib2.urlopen(url).read()
+filename = "btcdata.csv"
+if not os.path.exists(filename):
+    url = "https://api.blockchain.info/charts/market-price?timespan=all&sampled=true&metadata=false&cors=true&format=csv"
 
-with open('btcdata.csv','w') as f:
-    f.write(stream)
+    with urllib.request.urlopen(url) as response, open(filename, 'wb') as out_file:
+        data = response.read()
+        out_file.write(data)
 
-def getdatatable():
-    with open('btcdata.csv','r') as file:
-        lines = file.readlines()
-    return lines
+df = pd.read_csv(filename, header=None)
+df[0] = pd.to_datetime(df[0], format="%Y-%m-%d %H:%M:%S")
+fig = go.Figure(data=[go.Scatter(
+    x=df[0],
+    y=df[1],
+    mode='lines', )
+])
+fig.update_layout(
+    xaxis=dict(
+        rangeselector=dict(
+            buttons=list([
+                dict(count=5,
+                     label="5 years",
+                     step="year",
+                     stepmode="todate"),
+            ])
+        ),
+        rangeslider=dict(
+            visible=True,
+        ),
+    )
+)
 
-def average_price(list):
-    return (sum(list) / len(list))
+app.layout = html.Div([
+    html.H1("Bitcoin Annual Return on Investment Calculator:"),
+    dcc.Graph(figure=fig),
+    dcc.Slider(
+        id='year_slider',
+        min=2009,
+        max=2021,
+        value=2017,
+        step=1,
+        marks={
+            2009: {'label': '2009'},
+            2010: {'label': '2010'},
+            2011: {'label': '2011'},
+            2012: {'label': '2012'},
+            2013: {'label': '2013'},
+            2014: {'label': '2014'},
+            2015: {'label': '2015'},
+            2016: {'label': '2016'},
+            2017: {'label': '2017'},
+            2018: {'label': '2018'},
+            2019: {'label': '2019'},
+            2020: {'label': '2020'},
+            2021: {'label': '2021'}
+        },
+        included=False
+    ),
+    html.Br(),
+    html.H3(id='my-output'),
+])
 
-datatable = getdatatable()
-count = 0
-first_day = ''
-current_year = ''
-years_prices = list()
-returns_list = list()
-last_average = None
-firstyear = True
-print '** Bitcoin yearly performance **'
-print 'year .. price end of year .. avg for year .. performance'
-for line in datatable:
-    line = line.replace('\n','')
-    datetime, price = line.split(',')
-    date = datetime.split(' ')[0]
-    price = float(price)
 
-    if price == 0.0: continue
-    if(not first_day):
-        first_day = date
-    y, m, d = date.split('-')
+@app.callback(
+    Output('my-output', 'children'),
+    Input('year_slider', 'value')
+)
+def update_output_div(input_value):
+    today = date.today()
+    year_diff = int(today.year) - input_value
+    years_ago = (datetime.now() - relativedelta(years=year_diff))
+    current_price = float(df.iloc[-1, 1])
+    ago_price_index = df[0].searchsorted(years_ago)
+    ago_price = float(df.iloc[ago_price_index, 1])
 
-    if(not current_year):
-        current_year = y
-    years_prices.append(price)
-    count+=1
+    if ago_price > 0:
+        annual_return = (((current_price / ago_price) ** (1 / year_diff)) - 1) * 100
+        return f'From {years_ago.date()} {ago_price} to {today} {current_price}, annual return on investment is: {"%.2f" % annual_return}%'
+    else:
+        return 'Infinity'
 
-    if current_year != y: 
-        years_average = average_price(years_prices[:-1])
-        increase = 0
-        if last_average != None:
-            increase = (years_average / last_average) - 1
-            returns_list.append(increase)
-            last_average = years_average
-        else:
-            last_average = years_average
-        print current_year, '  ', format(years_prices[-2], '.6f'), '            ', round(years_average,3), '  ', round(increase,3)
-        current_year = y
-        years_prices = [years_prices[-1]]
 
-years_average = average_price(years_prices)
-increase = (years_average / last_average) - 1
-returns_list.append(increase)
-print current_year, '  ', format(years_prices[-1], '.6f'), '            ', round(years_average,3), '  ', round(increase,3)
-
-average_returns = average_price(returns_list)
-print 'total days: ',count
-print 'first day: ', first_day
-print 'last day: ', date
-print 'average return per year: ', round(average_returns  * 100, 5) , '%';
+if __name__ == "__main__":
+    app.run_server(debug=True)
